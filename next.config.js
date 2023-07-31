143a4db7bd8eda715354838acda6d5126f299506
GregorisB <gregorbachts@gmail.com>
2023-08-01 00:32:46 +0300

    changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
