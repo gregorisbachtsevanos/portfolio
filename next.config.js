844bc5183a7749d7c1da9ee9123bf362ec1c143b
GregorisB <gregorbachts@gmail.com>
2023-08-02 23:53:32 +0300

    changes next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,36 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
         domains: [
-            "t4.ftcdn.net"
+            't4.ftcdn.net',
+            'images.unsplash.com',
+            'placehold.co',
+        ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/600x400',
+            },
         ],
     },
+
+
+    // webpack(config) {
+    //     config.resolve.fallback = { fs: false };
+    //     config.module.rules.push({
+    //         test: /\.svg$/i,
+    //         issuer: /\.[jt]sx?$/,
+    //         use: ['@svgr/webpack'],
+    //     });
+
+    //     return config;
+    // },
+
 }
 
 module.exports = nextConfig
