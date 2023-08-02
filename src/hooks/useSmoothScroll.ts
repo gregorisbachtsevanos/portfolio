38c28a8bbba8e11c41651288176e14d5c7850f36
GregorisB <gregorbachts@gmail.com>
2023-08-03 00:46:57 +0300

    useSmoothScroll hook

---
+++ src/hooks/useSmoothScroll.ts
@@ -0,0 +1,36 @@
+import { useEffect, useCallback } from 'react';
+
+const useSmoothScroll = () => {
+  const handleScroll = useCallback((e: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    e.preventDefault();
+    const href = e.currentTarget.href;
+    if (!href) return;
+
+    const targetId = href.replace(/.*#/, '');
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleClick = (e: any): void => {
+      const isAnchorLink =
+        (e.target as HTMLElement).tagName.toLowerCase() === 'a' &&
+        (e.target as HTMLAnchorElement).href.includes('#');
+      if (isAnchorLink) {
+        handleScroll(e);
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [handleScroll]);
+
+  return handleScroll;
+};
+
+export default useSmoothScroll;
