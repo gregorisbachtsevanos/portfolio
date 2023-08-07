53d7c91bba13665e141d6bedba2f08737dc50f1f
GregorisB <gregorbachts@gmail.com>
2023-08-07 23:59:44 +0300

    visibility hook

---
+++ src/hooks/usePageVisibility.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const usePageVisibility = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleVisibilityChange = () => {
+    setIsVisible(!document.hidden);
+  };
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
+
+  return isVisible;
+};
+
+export default usePageVisibility;
