94f39ffdfe2ba68ec331da31b44efa59e8d75877
GregorisB <gregorbachts@gmail.com>
2023-08-12 10:46:41 +0300

    page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 'use client';
 import React from 'react';
-import Home from '@/features/Home/Home';
+import dynamic from 'next/dynamic';
+import { styled } from 'styled-components';
+
+// import Projects from '@/features/Projects/Projects';
+// import About from '@/features/About/About';
+// import Contact from '@/features/Contact/Contact';
+
+import usePageVisibility from '@/hooks/usePageVisibility';
+import PreloaderLayout from '@/layouts/PreloaderLayout';
+import PageLayout from '@/layouts/PageLayout';
 import { Sidebar } from '@/components/Sidebar';
-import Projects from '@/features/Projects/Projects';
+import { Preloader } from '@/components/Preloader/Preloader';
+import config from '@/config';
+
+const Home = dynamic(() => import('@/features/Home/'), { ssr: false });
+const Projects = dynamic(() => import('@/features/Projects/'), { ssr: false });
+const About = dynamic(() => import('@/features/About/'), { ssr: false });
+const Contact = dynamic(() => import('@/features/Contact/'), { ssr: false });
 
 const Page = () => {
+  const isVisible = usePageVisibility();
+  console.log(config);
   return (
     <>
-      <Sidebar />
-      <Home />
-      <Projects />
-      {/* <About /> */}
-      {/* <Contact /> */}
+      {true && (
+        <>
+          <PreloaderLayout withAnimation withOutScroll>
+            <Preloader />
+          </PreloaderLayout>
+          <PageLayout>
+            <Sidebar />
+            <Home />
+            <Projects />
+            <About />
+            <Contact />
+          </PageLayout>
+        </>
+      )}
     </>
   );
 };
