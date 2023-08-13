c00674e2cc032c757ab1ebd21cbc196dcb1883cd
Grigoris Bachtsevanos <grigoris.bachtsevanos@weare8.com>
2023-08-13 20:48:56 +0300

    email route

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,8 +1,10 @@
 import { EmailProps } from '@/types';
+import { sendEmail } from '@/utils/sendEmail';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  const { name, email, content }: Partial<EmailProps> = await request.json();
-  console.log(name, email, content);
-  return NextResponse.json({ name, email, content });
+  const { subject, html, email } = await request.json();
+  await sendEmail(subject, html, email);
+  console.log(subject, html, email);
+  return NextResponse.json({ subject, html, email });
 }
