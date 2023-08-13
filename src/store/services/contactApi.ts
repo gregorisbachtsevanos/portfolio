649fa3c8f5cf3cada4c26f2e72cd6aa5720bf636
Grigoris Bachtsevanos <grigoris.bachtsevanos@weare8.com>
2023-08-13 20:49:19 +0300

    contactAPI

--- src/store/services/contactApi.ts
+++ src/store/services/contactApi.ts
@@ -7,10 +7,10 @@ const CONTACT_API = config.contactAPI;
 export const contactApi = api.injectEndpoints({
   endpoints: (build) => ({
     postEmail: build.mutation({
-      query: ({ name, email, content }) => ({
+      query: ({ subject, html, email }) => ({
         url: `${CONTACT_API}`,
         method: 'POST',
-        body: { name, email, content },
+        body: { subject, html, email },
       }),
     }),
   }),
