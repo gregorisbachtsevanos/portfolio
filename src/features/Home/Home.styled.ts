3460aff2137eb921362e9fe92d28f21aca138cb8
Grigoris Bachtsevanos <grigoris.bachtsevanos@weare8.com>
2023-08-14 18:09:22 +0300

    section height changes

--- src/features/Home/Home.styled.ts
+++ src/features/Home/Home.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledHomeContainer = styled.div`
-  height: 100%;
+  height: 100vh;
 
   .home-container {
     position: relative;
--- src/features/Projects/Projects.styled.ts
+++ src/features/Projects/Projects.styled.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledProjectsContainer = styled.div`
-  height: 100%;
+  height: 100vh;
+
   display: flex;
   align-items: center;
 
