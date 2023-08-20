79c9911f312c05467c00d916877a0274545d8563
Grigoris Bachtsevanos <grigoris.bachtsevanos@weare8.com>
2023-08-20 16:57:35 +0300

    changes

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,31 +1,32 @@
-import "styled-components";
+import 'styled-components';
 
-declare module "styled-components" {
-	export interface DefaultTheme {
-		palette: {
-			black: {
-				1: string;
-				2: string;
-				3: string;
-				4: string;
-			};
-			highlight: {
-				1: string;
-				2: string;
-				3: string;
-			};
-			white: string;
-			success: string;
-			error: string;
-		};
-		sizes: {
-			smallMobile: string;
-			mobile: string;
-			tablet: string;
-			laptop: string;
-			desktop: string;
-		};
-		maxWidth: string;
-		pagePadding: string;
-	}
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    palette: {
+      black: {
+        1: string;
+        2: string;
+        3: string;
+        4: string;
+      };
+      highlight: {
+        1: string;
+        2: string;
+        3: string;
+      };
+      white: string;
+      success: string;
+      error: string;
+    };
+    sizes: {
+      mobileS: string;
+      mobileM: string;
+      mobileL: string;
+      tablet: string;
+      laptop: string;
+      desktop: string;
+    };
+    maxWidth: string;
+    pagePadding: string;
+  }
 }
