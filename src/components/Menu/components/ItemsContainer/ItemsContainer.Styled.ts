f37d037b41702b1bed9fa945eb6cd80a7ed11532
GregorisB <gregorbachts@gmail.com>
2023-08-29 23:29:40 +0300

    items container

---
+++ src/components/Menu/components/ItemsContainer/ItemsContainer.Styled.ts
@@ -0,0 +1,3 @@
+import { styled } from 'styled-components';
+
+export const StyledItemsContainer = styled.div``;
---
+++ src/components/Menu/components/ItemsContainer/ItemsContainer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ItemsContainer = () => {
+  return <div>ItemsContainer</div>;
+};
