a370dd5eac0a8c6f4c47f5e5c272c98716745c84
GregorisB <gregorbachts@gmail.com>
2023-08-29 23:29:03 +0300

    menu component

--- src/components/Menu/Menu.styled.ts
+++ src/components/Menu/Menu.styled.ts
@@ -1,9 +1,33 @@
 import { styled } from 'styled-components';
 
 export const StyledMenuContainer = styled.nav`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  padding: 0 7px;
-  background-color: transparent;
+  .menu-icons {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    padding: 0 17px;
+    background-color: transparent;
+  }
+
+  .items-container {
+    width: 100%;
+    height: 100%;
+    left: 0;
+    bottom: 0;
+    background-color: ${({ theme }) => theme.palette.black[4]};
+    position: absolute;
+    z-index: 99;
+
+    .close-icon {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      padding: 0 17px;
+    }
+  }
+
+  img {
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+  }
 `;
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { StyledMenuContainer } from './Menu.styled';
 import Image from 'next/image';
 import SvgIcon from '../SvgIcon/SvgIcon';
+import { ContactForm } from '../ContactForm';
 
 export const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = useCallback(() => {
+    setShowMenu((prev: any) => !prev);
+  }, []);
   return (
     <StyledMenuContainer>
-      <SvgIcon type="menu" />
+      <div className="menu-icons" onClick={toggleMenu}>
+        <SvgIcon type="menu" />
+      </div>
+
+      {showMenu && (
+        <div className="items-container">
+          <div className="close-icon" onClick={toggleMenu}>
+            <SvgIcon type="close" />
+          </div>
+          <div className="items">
+            <ContactForm />
+          </div>
+        </div>
+      )}
     </StyledMenuContainer>
   );
 };
