2ff0008830cd36c6ba600871f6deb4c60f2a6921
GregorisB <gregorbachts@gmail.com>
2023-09-06 23:55:59 +0300

    changes

--- src/store/services/dataApi.ts
+++ src/store/services/dataApi.ts
@@ -1,6 +1,6 @@
 import config from '@/config';
 import { api } from './api/';
-import { UsersInfo } from '@/types';
+import { Project, UsersInfo } from '@/types';
 
 const DATA_API = config.dataAPI;
 
@@ -8,7 +8,13 @@ export const dataApi = api.injectEndpoints({
   endpoints: (build) => ({
     getUsersInfo: build.query<UsersInfo, void>({
       query: () => ({
-        url: `${DATA_API}/`,
+        url: `${DATA_API}/user_info`,
+        method: 'GET',
+      }),
+    }),
+    getProjects: build.query<Project[], void>({
+      query: () => ({
+        url: `${DATA_API}/projects`,
         method: 'GET',
       }),
     }),
