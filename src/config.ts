00da626e17b22ac08174718e4aec62b68f2f1b7b
GregorisB <gregorbachts@gmail.com>
2023-09-10 23:36:24 +0300

    changes

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ const config = {
   contactAPI: process.env.NEXT_PUBLIC_CONTACT_API,
   nodemailerPassword: process.env.NEXT_PUBLIC_NODEMAILER_PASSWORD,
   nodemailerEmail: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+  mongoDB: process.env.NEXT_PUBLIC_MONGODB,
   auth: {
     clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
   },
