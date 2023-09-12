0ff0e71a68719c25a1380b2f0796d4555c5ee6ad
GregorisB <gregorbachts@gmail.com>
2023-09-12 22:09:34 +0300

    changeds

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,22 @@
-const config = {
+export const config = {
   environment: process.env.NEXT_PUBLIC_ENV,
   dataAPI: process.env.NEXT_PUBLIC_DATA_API,
   contactAPI: process.env.NEXT_PUBLIC_CONTACT_API,
-  nodemailerPassword: process.env.NEXT_PUBLIC_NODEMAILER_PASSWORD,
-  nodemailerEmail: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
   mongoDB: process.env.NEXT_PUBLIC_MONGODB,
+  nodemailer: {
+    password: process.env.NEXT_PUBLIC_NODEMAILER_PASSWORD,
+    email: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+  },
   auth: {
     clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
   },
+  cors: {
+    method: process.env.ALLOWED_METHODS,
+    origin: process.env.ALLOWED_ORIGIN,
+    allowedHeaders: process.env.ALLOWED_HEADERS,
+    exposedHeaders: process.env.EXPOSED_HEADERS,
+    maxAge: process.env.MAX_AGE,
+    credentials: process.env.CREDENTIALS,
+    domain: process.env.DOMAIN_URL,
+  },
 };
-
-export default config;
