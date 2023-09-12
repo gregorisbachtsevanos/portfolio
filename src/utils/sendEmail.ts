a0fc514cec508c22f9fabefa195b39d3e82d9688
GregorisB <gregorbachts@gmail.com>
2023-09-12 23:44:59 +0300

    nodemailer changes

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,19 +1,19 @@
-import config from '@/config';
+import { config } from '@/config';
 import nodemailer from 'nodemailer';
 
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   service: 'gmail', // e.g., 'Gmail', 'Outlook'
   auth: {
-    user: config.nodemailerEmail,
-    pass: config.nodemailerPassword,
+    user: config.nodemailer.email,
+    pass: config.nodemailer.password,
   },
 });
 
 export const sendEmail = async (subject: string, html: string, replyTo: string, from?: string) => {
   const mailOptions = {
-    from: from ?? config.nodemailerEmail,
-    to: config.nodemailerEmail,
+    from: from ?? config.nodemailer.email,
+    to: config.nodemailer.email,
     subject,
     html,
     replyTo,
