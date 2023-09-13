f4389c8cefec4eab85209c1bf0d4b8470f8b6f88
GregorisB <gregorbachts@gmail.com>
2023-09-14 00:10:58 +0300

    api response format changes

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -6,5 +6,5 @@ export async function GET(request: NextRequest) {
   const client = await clientPromise;
   const db = client?.db('portfolio');
   const user = await db.collection('en').findOne({});
-  return NextResponse.json({ user });
+  return NextResponse.json(user);
 }
