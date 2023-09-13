39ed34bd7c025b70c7ad634abb3a4be93f359407
GregorisB <gregorbachts@gmail.com>
2023-09-13 21:32:06 +0300

    changes

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(request: Request) {
   const { subject, html, email } = await request.json();
   await sendEmail(subject, html, email);
-  console.log(subject, html, email);
+  console.log('email request data: ', subject, html, email);
   // TODO: save request data (subject, html, email etc.) to db
   return NextResponse.json({ subject, html, email });
 }
