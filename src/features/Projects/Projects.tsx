9b91b768c05c833964fe22c458ee161a1d39b611
GregorisB <gregorbachts@gmail.com>
2023-09-14 00:01:17 +0300

    load projects from the db

--- src/features/Projects/Projects.tsx
+++ src/features/Projects/Projects.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Project from './components/Project';
 
 import { routes } from '@/constants/routes';
 import { StyledProjectsContainer } from './Projects.styled';
 import { projectsData } from './constants';
+import { useGetProjectsQuery } from '@/store/services/projectApi';
+import { selectUserProject } from '@/store/state/userInfoSlice';
+import { useSelector } from 'react-redux';
 
 const Projects = () => {
-  const data = projectsData;
+  const { data: projectData, isLoading, isSuccess } = useGetProjectsQuery();
   return (
     <StyledProjectsContainer id={routes.PROJECTS}>
       <div className="container">
         <div className="projects">
-          {data.map((project, index) => (
-            <Project
-              key={project?.id}
-              title={project.title}
-              link={project.link}
-              image={project.image}
-            />
-          ))}
+          {isLoading ? (
+            <></>
+          ) : (
+            projectData?.map((project, index) => (
+              <Project
+                key={project.title}
+                title={project.title}
+                link={project.link}
+                image={project.images}
+              />
+            ))
+          )}
         </div>
       </div>
     </StyledProjectsContainer>
