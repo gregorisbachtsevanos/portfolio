77ceddea3f92c96deedad5e936dc767fa99a6082
GregorisB <gregorbachts@gmail.com>
2023-09-17 23:06:13 +0300

    redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { api } from "./services/api/api";
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from './services/api/api';
+import userReducer from './state/userInfoSlice';
 
 export const store = configureStore({
-	reducer: { [api.reducerPath]: api.reducer },
-	devTools: process.env.NODE_ENV !== "production",
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(api.middleware),
+  reducer: {
+    user: userReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
