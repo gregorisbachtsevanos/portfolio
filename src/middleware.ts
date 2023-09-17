3a4302eab5a673270653963780e144e2fd8dd453
GregorisB <gregorbachts@gmail.com>
2023-09-17 23:06:42 +0300

    cors middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,6 +28,7 @@ export async function middleware(request: NextRequest) {
   if (corsOptions.allowedOrigins.includes('*') || corsOptions.allowedOrigins.includes(origin))
     response.headers.set('Access-Control-Allow-Origin', origin);
 
+  response.headers.set('Access-Control-Allow-Origin', '*');
   // Set default CORS headers
   response.headers.set('Access-Control-Allow-Credentials', corsOptions.credentials.toString());
   response.headers.set('Access-Control-Allow-Methods', corsOptions.allowedMethods.join(','));
