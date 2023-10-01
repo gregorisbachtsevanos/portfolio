682c93509c5a343adf4e4f3eb89ed58898bce422
GregorisB <gregorbachts@gmail.com>
2023-10-01 23:31:15 +0300

    CLEAN UP

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React, { useMemo } from 'react';
 import Scroll from 'react-scroll';
-
-import { logoUrl, sidebar } from './constants';
-import { StyledSidebarContainer } from './Sidebar.styled';
-import { Title } from '@/app/theme';
-import Image from 'next/image';
 import Link from 'next/link';
-import useSmoothScroll from '@/hooks/useSmoothScroll';
-import SvgIcon from '../SvgIcon/SvgIcon';
+
+import { Title } from '@/app/theme';
+
 import useWindowSize from '@/hooks/useWindowSize';
+import useSmoothScroll from '@/hooks/useSmoothScroll';
+
+import { sidebar } from './constants';
 import { tabletView } from '@/constants/data';
+
+import SvgIcon from '../SvgIcon/SvgIcon';
+import { StyledSidebarContainer } from './Sidebar.styled';
 
 const SmoothLink = Scroll.Link;
 
