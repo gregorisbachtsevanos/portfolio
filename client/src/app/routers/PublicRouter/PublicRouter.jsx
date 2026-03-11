b65a2715b8e5240814e82842c9944d23fa1c25ff
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2026-03-11 10:00:31 +0200

    Delete client directory

--- client/src/app/routers/PublicRouter/PublicRouter.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { getCookie } from "src/utils/storage";
-
-export const PublicRouter = () => {
-	const accessToken = getCookie("accessToken");
-	console.log(accessToken);
-	return !accessToken ? <Outlet /> : <Navigate to="/dashboard" />;
-};
