61328b0adf3d6e7b6ae53ed2c5a2508bde797df3
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-03-22 11:46:04 +0200

    clear site config image

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
-	name: "Gregoris Bachtsevanos",
-	email: "contact@example.com",
-	githubUrl: "https://github.com",
-	linkedinUrl: "https://linkedin.com",
-	location: "Greece",
-	ogImageUrl: "https://bolt.new/static/og_default.png",
+  name: "Gregoris Bachtsevanos",
+  email: "contact@example.com",
+  githubUrl: "https://github.com",
+  linkedinUrl: "https://linkedin.com",
+  location: "Greece",
+  ogImageUrl: " ",
 } as const;
