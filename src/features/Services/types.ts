22d61ed7c0fad0c6dc5e398650742d0744a698c7
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-05-06 15:58:06 +0300

    dlt files

--- src/features/Services/types.ts
+++
@@ -1,9 +0,0 @@
-import { IMessages, TServiceId } from "@/lang";
-import { LucideIcon } from "lucide-react";
-
-type TServiceItem = IMessages["services"]["items"][TServiceId];
-
-export type ServiceProps = {
-	serviceItem: TServiceItem;
-	Icon: LucideIcon;
-};
